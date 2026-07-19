8dfcdeb983703d5c289e0caff1e5735051e715cf
Ibrahim Gad <yehya2154@gmail.com>
2026-07-19 17:21:50 +0300

    Add redis cache configuration and dependencies and handling docker

--- src/main/java/gov/iti/jets/NutriScan/service/AllergyService.java
+++ src/main/java/gov/iti/jets/NutriScan/service/AllergyService.java
@@ -8,6 +8,8 @@ import gov.iti.jets.NutriScan.mapper.AllergyMapper;
 import gov.iti.jets.NutriScan.model.Allergy;
 import gov.iti.jets.NutriScan.repository.AllergyRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -19,12 +21,14 @@ public class AllergyService {
     private final AllergyRepository allergyRepository;
     private final AllergyMapper allergyMapper;
 
+    @Cacheable(value = "allergies", key = "#id")
     public AllergyResponse findById(Integer id) {
         return allergyRepository.findById(id)
             .map(allergyMapper::toResponse)
             .orElseThrow(() -> new AllergyNotFoundException("Allergy not found with id: " + id));
     }
 
+    @Cacheable(value = "allergies", key = "#name")
     public AllergyResponse findByName(String name) {
         return allergyRepository.findByName(name)
             .map(allergyMapper::toResponse)
@@ -32,10 +36,12 @@ public class AllergyService {
                 () -> new AllergyNotFoundException("Allergy not found with name: " + name));
     }
 
+    @Cacheable(value = "allergies", key = "'all'")
     public List<AllergyResponse> findAll() {
         return allergyMapper.toResponseList(allergyRepository.findAll());
     }
 
+    @CacheEvict(value = "allergies", allEntries = true)
     public AllergyResponse save(AllergyRequest allergyRequest) {
         if (allergyRepository.existsByName(allergyRequest.name())) {
             throw new AllergyConflictException(
@@ -46,6 +52,7 @@ public class AllergyService {
         return allergyMapper.toResponse(allergyRepository.save(allergy));
     }
 
+    @CacheEvict(value = "allergies", allEntries = true)
     public List<AllergyResponse> saveAll(List<AllergyRequest> allergyRequests) {
 
         for (var allergyRequest : allergyRequests) {
@@ -58,7 +65,7 @@ public class AllergyService {
         List<Allergy> allergies = allergyMapper.toEntityList(allergyRequests);
         return allergyMapper.toResponseList(allergyRepository.saveAll(allergies));
     }
-
+    @CacheEvict(value = "allergies", allEntries = true)
     public void delete(Integer id) {
         allergyRepository.deleteById(id);
     }
