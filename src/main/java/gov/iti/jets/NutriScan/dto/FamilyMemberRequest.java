412761bf345decb29b01fe5eff5dfa92cea392fb
Ibrahim-Soliman0 <ibrahimsoliman269@gmail.com>
2026-07-23 14:53:25 +0300

    add: relation field in family members

--- src/main/java/gov/iti/jets/NutriScan/dto/FamilyMemberRequest.java
+++ src/main/java/gov/iti/jets/NutriScan/dto/FamilyMemberRequest.java
@@ -7,7 +7,8 @@ import jakarta.validation.constraints.Size;
 import java.util.List;
 
 public record FamilyMemberRequest(
-
-    @NotBlank @Size(max = 150) String name, List<@NotNull Integer> allergyIds,
-    List<@NotNull Integer> diseaseIds) {
+        @NotBlank @Size(max = 150) String name,
+        @NotBlank @Size(max = 150) String relation,
+        List<@NotNull Integer> allergyIds,
+        List<@NotNull Integer> diseaseIds) {
 }
